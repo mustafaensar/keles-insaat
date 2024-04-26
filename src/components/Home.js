760cb909f0da20e1c0d40ab8e4b2709a10d1a95e
Mustafa Ensar <57106574+mustafaensar@users.noreply.github.com>
2024-04-26 00:18:59 -0500

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -8,7 +8,7 @@ export default function Home() {
           <div className="container">
             <div className="row justify-content-center">
               <div className="col-lg-6 text-center">
-                <h2><span>Write Information </span><br/>About the Company</h2>
+                <h2>Write Information<br/>About the Company</h2>
                 <p>Details and images about your company</p>
                 <a href="/gallery" className="btn-get-started">PROJECTS</a>
               </div>
